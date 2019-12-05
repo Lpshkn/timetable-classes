2aacd30e00fc1d4d3d230970b17452b2c48c23ce
lpshkn <kofffsh@gmail.com>
2019-12-05 17:51:30 +0300

    Some updates, added setup.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Timetable of classes
+##Description
 This is a parser of JSON files to Excel table.
+
+##Usage
+- ```git clone https://github.com/Nikshepel/timetable-classes.git```
+
+
--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from src.parser import parse_json
 from src.excel_write import write
 from src.data_json import load_data
 
-jsonObj = load_data('./../resources/test.json')
-days = parse_json(jsonObj)
-write(days)
+def main():
+    jsonObj = load_data('./../resources/test.json')
+    days = parse_json(jsonObj)
+    write(days)
 
+if __name__ == '__main__':
+    main()
 
