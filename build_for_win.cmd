a6cd70e9b9729360fc67858d951b8c24908c5bb9
LeadNess <leadness@mail.ru>
2019-12-25 04:18:21 +0300

    Fixed build_for_win script

--- build_for_win.cmd
+++ build_for_win.cmd
@@ -1,6 +1,6 @@
 python -m venv venv
 echo Created virtual environment...
-venv\Scripts\activate
+.\venv\Scripts\activate
 pip install -r requirements.txt
 pyinstaller --onefile TimetableExcelParser.py
 echo Successfully build .\dist\TimetableExcelParser.exe
