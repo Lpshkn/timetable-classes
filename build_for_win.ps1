e15cd0a09c384c4e1fc53da4751b5cfc496d072c
LeadNess <leadness@mail.ru>
2019-12-25 04:27:01 +0300

    build_for_win script: .cmd => .ps1

--- build_for_win.ps1
+++ build_for_win.ps1
@@ -1,6 +1,6 @@
 python -m venv venv
-echo Created virtual environment...
+Write-Host "Created virtual environment..."
 .\venv\Scripts\activate
 pip install -r requirements.txt
 pyinstaller --onefile TimetableExcelParser.py
-echo Successfully build .\dist\TimetableExcelParser.exe
+Write-Host "Successfully build .\dist\TimetableExcelParser.exe"
